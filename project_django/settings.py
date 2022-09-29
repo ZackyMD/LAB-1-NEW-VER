4d71e9af6a4235414625744eb3aad3aa4d25843f
Your Namegit <you@example.com>
2022-09-29 13:41:06 +0700

    Penambahan Navbar

--- project_django/settings.py
+++ project_django/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# Static files (CSS, JavaScript, Images)
+# httpsdocs.djangoproject.comen1.9howtostatic-files
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
